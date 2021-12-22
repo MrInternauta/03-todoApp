043588fd2fbc74a4424df45a1b84659ce57d1484
Felipe de Jesus Ramirez Rodriguez <feldjesus@gmail.com>
2021-12-22 05:53:10 -0700

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoFooterComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
